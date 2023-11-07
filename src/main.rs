b0f50cc77d84edee510b7ad660a9525f5cf4943e
Matt Cain <matt@madebywink.com>
2023-11-06 23:59:46 -0700

    split api mod

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use axum::{routing::get, Router};
+pub mod api;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Boat broker!" }));
-
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+        .serve(api::router().into_make_service())
         .await
         .unwrap();
 }
