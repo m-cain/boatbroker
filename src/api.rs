a59d90d95cf3cd70429ae3cc34e859b9acb3fa65
Matt Cain <matt@madebywink.com>
2023-11-07 22:00:47 -0700

    adds graceful shutdowns

--- src/api.rs
+++ src/api.rs
@@ -1,21 +1,31 @@
 use crate::auth;
 use crate::state::AppState;
 use axum::Router;
-use std::{error::Error, net::SocketAddr};
+use std::{future::Future, net::SocketAddr};
 
 fn routes(state: AppState) -> Router {
     Router::new().nest("/auth", auth::routes(state))
 }
 
-pub async fn serve() -> Result<(), Box<dyn Error>> {
+async fn shutdown(sig: impl Future<Output = ()>, state: AppState) {
+    sig.await;
+    println!("Shutting down...");
+    state.db_pool.close().await;
+}
+
+pub async fn serve<S: Future<Output = ()>>(sig: S) -> () {
     let state = AppState::from_env().await;
     let routes = routes(state.clone());
 
     let port = state.settings.port;
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
-    axum::Server::bind(&addr)
+    let graceful = axum::Server::bind(&addr)
         .serve(routes.into_make_service())
-        .await
-        .map_err(|e| e.into())
+        .with_graceful_shutdown(shutdown(sig, state))
+        .await;
+
+    if let Err(e) = graceful {
+        eprintln!("server error: {}", e);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,29 @@ pub mod api;
 pub mod auth;
 pub mod state;
 
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install CTRL+C signal handler");
+    };
+
+    let termination = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install termination signal handler")
+            .recv()
+            .await
+            .expect("failed to receive termination signal")
+    };
+
+    tokio::select! {
+        _ = ctrl_c => println!("CTRL+C received"),
+        _ = termination => println!("Termination signal received"),
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    api::serve().await.unwrap();
+    let sig = shutdown_signal();
+    api::serve(sig).await;
 }
